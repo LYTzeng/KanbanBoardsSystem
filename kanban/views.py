98b1a27af82e7884540a651b6c313e059c8a5988
KevinShih707 <d122784090@gmail.com>
2018-10-31 17:46:01 +0800

    push test

--- kanban/views.py
+++ kanban/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.template import RequestContext
 
-
+#kevin
 class KBboard(View):
 
     def get(self, request):
