30885b756700aa1910960cee4b57c03734f32025
Oscar Tzeng <oscar217b@gmail.com>
2018-10-31 11:17:58 +0800

    改善看板樣式&加上註解

--- static/js/kb/basic-board.js
+++ static/js/kb/basic-board.js
@@ -2,7 +2,6 @@ var colorArray = ['#556270', '#4ecdc4', '#c7f464', '#ff6b6b', '#c44d58'];
 var textColor = ['white','black','black','white','white'];
 
 $(document).ready(function () {
-
     var color = d3.scaleOrdinal()
                 .range(colorArray); 
 
@@ -13,7 +12,7 @@ $(document).ready(function () {
         });
         }
 
-    var fields = [
+    var fields = [// 資料型態
              { name: "id", type: "string" },
              { name: "status", map: "state", type: "string" },
              { name: "text", map: "label", type: "string" },
@@ -23,15 +22,15 @@ $(document).ready(function () {
     ];
     var source =
      {
-         localData: [
-                  { id: "1161", state: "new", label: "SRS撰寫", tags: "SRS", hex: color(getRandom(1,5))},
-                  { id: "1645", state: "work", label: "Prototyping", tags: "Prototype", hex: color(getRandom(1,5))},
-                  { id: "9213", state: "new", label: "登入驗證開發與測試", tags: "登入,開發", hex: color(getRandom(1,5))},
-                  { id: "6546", state: "done", label: "登入驗證設計", tags: "登入, 設計", hex: color(getRandom(1,5))},
-                  { id: "9034", state: "new", label: "UI Design", tags: "UI, 設計", hex: color(getRandom(1,5))},
-                  { id: "9034", state: "new", label: "資料庫設計", tags: "資料庫設計, 設計", hex: color(getRandom(1,5))},
-                  { id: "9034", state: "new", label: "專案選單數計", tags: "選單, 設計", hex: color(getRandom(1,5))},
-                  { id: "9034", state: "new", label: "任務管理設計", tags: "任務管理, 設計", hex: color(getRandom(1,5))},
+         localData: [// Fake Data
+                  { id: getRandom(1000,5000), state: "new", label: "SRS撰寫", tags: "SRS", hex: color(getRandom(1,5))},
+                  { id: getRandom(1000,5000), state: "work", label: "Prototyping", tags: "Prototype", hex: color(getRandom(1,5))},
+                  { id: getRandom(1000,5000), state: "new", label: "登入驗證開發與測試", tags: "登入,開發", hex: color(getRandom(1,5))},
+                  { id: getRandom(1000,5000), state: "done", label: "登入驗證設計", tags: "登入, 設計", hex: color(getRandom(1,5))},
+                  { id: getRandom(1000,5000), state: "new", label: "UI Design", tags: "UI, 設計", hex: color(getRandom(1,5))},
+                  { id: getRandom(1000,5000), state: "new", label: "資料庫設計", tags: "資料庫設計, 設計", hex: color(getRandom(1,5))},
+                  { id: getRandom(1000,5000), state: "new", label: "專案選單數計", tags: "選單, 設計", hex: color(getRandom(1,5))},
+                  { id: getRandom(1000,5000), state: "new", label: "任務管理設計", tags: "任務管理, 設計", hex: color(getRandom(1,5))},
          ],
          dataType: "array",
          dataFields: fields
@@ -40,7 +39,7 @@ $(document).ready(function () {
     var resourcesAdapterFunc = function () {
         var resourcesSource =
         {
-            localData: [
+            localData: [// Fake Data
                   { id: 0, name: "No name",  common: true },
                   { id: 1, name: "Andrew Fuller"},
                   { id: 2, name: "Janet Leverling" },
@@ -53,7 +52,7 @@ $(document).ready(function () {
                   { id: 9, name: "Laura Buchanan"  }
             ],
             dataType: "array",
-            dataFields: [
+            dataFields: [// 資料型態
                  { name: "id", type: "number" },
                  { name: "name", type: "string" },
                  { name: "image", type: "string" },
@@ -67,27 +66,89 @@ $(document).ready(function () {
     var getIconClassName = function () {
         return "jqx-icon-plus-alt";
     }
-
+    
+    // 畫出看板
     $('#kanban').jqxKanban({
         resources: resourcesAdapterFunc(),
         source: dataAdapter,
+        // 自訂Task卡片物件的模板
+        template: "<div class='jqx-kanban-item'>"
+        + "<div class='jqx-kanban-item-color-status'></div>"
+        + "<div class='jqx-kanban-item-text'></div>"
+        + "<div class='jqx-kanban-item-footer'></div>"
+        + "</div>",
         theme: "light",
-      
+        height: "494px",
+        width: '800px',
+        // 定義卡片樣式
+        itemRenderer: function(element, item, resource){
+                $(element)
+                    .css('background', item.color)// Task卡片底色
+                    .css('border-style', 'none')
+                    .css('border-radius', '4px')
+                    .css('margin-top', '12px')
+                    .css('margin-bottom', '12px')
+                    // 拖曳陰影效果
+                    .mouseup(function (e) {// 滑鼠按下
+                        $(e.target).closest('.jqx-kanban-item').css('box-shadow', '0 2px 4px 0 rgba(0, 0, 0, 0.1), 0 2px 5px 0 rgba(0, 0, 0, 0.089)');
+                    })
+                    .mousedown(function(e) {// 放開滑鼠
+                        $(e.target).closest('.jqx-kanban-item').css('box-shadow', '0 8px 20px 5px rgba(0, 0, 0, 0.25)');
+                    });
+                $(element).find(".jqx-kanban-item-footer").css('border-top-color', item.color);// 卡片上色
+                $(element).find(".jqx-kanban-item-text").css('color', textColor[colorArray.indexOf(item.color)]);// 卡片文字顏色
+        },
+        // 定義看板行
         columns: [
             { text: "TO DO", iconClassName: getIconClassName(), dataField: "new" },
-            { text: "In Progress", iconClassName: getIconClassName(), dataField: "work" },
+            { text: "In Progress", iconClassName: getIconClassName(), dataField: "work" , maxItems: 5},
             { text: "Done", iconClassName: getIconClassName(), dataField: "done" }
-        ]
+        ],
+        // 看板行標題樣式
+        columnRenderer: function (element, collapsedElement, column) {
+            var columnItems = $("#kanban").jqxKanban('getColumnItems', column.dataField).length;
+            element.find(".jqx-kanban-column-header-title")
+            .css('height', '50px')
+            .css('line-height', '50px');
+            if (column.dataField == "work"){
+                // update header's status.
+                element.find(".jqx-kanban-column-header-title").html("<p style='line-height: 25px;'>" + column.text + "<br><x class='work-items'>" + columnItems + "/" + (column.maxItems-1) + "</x></p>");
+                // update collapsed header's status.
+                collapsedElement.find(".jqx-kanban-column-header-title").html(column.text);
+                collapsedElement.find(".jqx-kanban-column-header-status").html("<x class='work-items'>  " + columnItems + "/" + (column.maxItems-1) + "</x>");
+                // 數量滿了變成紅字
+                workItems = $('.work-items');
+                if (columnItems >= column.maxItems)
+                    workItems.css('color', 'red');
+                else
+                    workItems.css('color', '');
+            }
+            // else {
+            //     element.find(".jqx-kanban-column-header-title").html("<p>" + column.text + "</p>");
+            // }
+            $(".jqx-kanban-column-header-title").css('left', '-80px');
+            $(".jqx-kanban-column-header-status").css('left', '-80px');
+        }
     }); 
-    var itemIndex = 0;
-    // 新增Task
+    var itemIndex = 0;// 新增卡片的編號從0開始
+    // 新增Task卡片
     $('#kanban').on('columnAttrClicked', function (event) {
         var args = event.args;
         if (args.attribute == "button") {
             args.cancelToggle = true;
             if (!args.column.collapsed) {
+                // 新增卡片的顏色
                 var inputColor = color(getRandom(1,5));
-                $('#kanban').jqxKanban('addItem', { status: args.column.dataField, text: "<input placeholder='Input task name here.' style='width: 96%; margin-top:2px; border-color: transparent; text-align: center; color: "+ textColor[colorArray.indexOf(inputColor)] +"; line-height:20px; height: 20px;' class='jqx-input' id='newItem" + itemIndex + "' value=''/>", tags: "new task", color: inputColor, resourceId: Math.floor(Math.random() * 4), className: "new-task" });
+                // 點選+號要做的事：新增卡片
+                $('#kanban').jqxKanban('addItem', { 
+                    status: args.column.dataField, 
+                    text: "<input placeholder='Input task name here.' style='width: 96%; margin-top:2px; border-color: transparent; text-align: center; color: "+ textColor[colorArray.indexOf(inputColor)] +"; line-height:20px; height: 20px;' class='jqx-input' id='newItem" + itemIndex + "' value=''/>", 
+                    tags: "new task", 
+                    color: inputColor, 
+                    resourceId: Math.floor(Math.random() * 4), 
+                    className: "new-task" 
+                });
+                // 打字的地方
                 var input =  $("#newItem" + itemIndex);
                 input.mousedown(function (event) {
                     event.stopPropagation();
@@ -95,7 +156,6 @@ $(document).ready(function () {
                 input.mouseup(function (event) {
                     event.stopPropagation();
                 });
-
                 input.keydown(function (event) {
                     if (event.keyCode == 13) {
                         $("<span>" + $(event.target).val() + "</span>").insertBefore($(event.target));
@@ -105,59 +165,27 @@ $(document).ready(function () {
                 input.focus();
                 itemIndex++;
                 // 新增的Task一樣要改成自訂的樣式
-                $('.new-task > .jqx-kanban-item-avatar').remove();
-                $('.new-task > .jqx-kanban-item-footer').css('border-top-color', inputColor);
-                $('.new-task')
-                    .css('border-style', 'none')
-                    .css('border-radius', '4px')
-                    .css('margin-top', '8px')
-                    .css('margin-bottom', '8px')
-                    .css('background-color', inputColor)
-                    .mouseup(function (e) {
-                        $(e.target).closest('.jqx-kanban-item').css('box-shadow', '0 2px 4px 0 rgba(0, 0, 0, 0.1), 0 2px 5px 0 rgba(0, 0, 0, 0.089)');
-                    })
-                    .mousedown(function(e) {
-                        $(e.target).closest('.jqx-kanban-item').css('box-shadow', '0 6px 25px 0 rgba(0, 0, 0, 0.25)');
-                    })
                 $('.new-task > .jqx-kanban-item-text')
                     .css('color', textColor[colorArray.indexOf(inputColor)])
                     .addClass('kanban-item-text')
                     .removeClass('jqx-kanban-item-text');
                 $('.new-task > .jqx-kanban-item-color-status').remove();
-                $('.new-task').removeClass('new-task');
-                
+                $('.new-task').removeClass('new-task');  
             }
         }
     });
 
     // 以下是把jQWidget預設樣式改掉的部分
-    $('.jqx-kanban-item-avatar').remove();
-    $('.jqx-kanban-column-header').css('height', '36.652px');
-    $('.jqx-kanban-item')
-        .css('border-style', 'none')
-        .css('border-radius', '4px')
-        .css('margin-top', '8px')
-        .css('margin-bottom', '8px')
-        .mouseup(function (e) {
-            $(e.target).closest('.jqx-kanban-item').css('box-shadow', '0 2px 4px 0 rgba(0, 0, 0, 0.1), 0 2px 5px 0 rgba(0, 0, 0, 0.089)');
-        })
-        .mousedown(function(e) {
-            $(e.target).closest('.jqx-kanban-item').css('box-shadow', '0 8px 20px 5px rgba(0, 0, 0, 0.25)');
-        });
-    var kanbanItem = document.getElementsByClassName('jqx-kanban-item');
-    var kanbanColor = document.getElementsByClassName('jqx-kanban-item-color-status');
-    var text = document.getElementsByClassName('jqx-kanban-item-text');
-    var kanbanBorder = document.getElementsByClassName('jqx-kanban-item-footer');
-    var i = 0;
-    for(i = 0 ; i<kanbanItem.length ; i++){
-        bgColor = kanbanColor[i].style.backgroundColor
-        kanbanItem[i].style.backgroundColor = bgColor;
-        kanbanBorder[i].style.borderTopColor = bgColor;
-        text[i].style.color = textColor[colorArray.indexOf(rgbToHex(bgColor))];
-    }
+    $('.jqx-kanban-column-header').css('height', '50px');
+    $('.jqx-kanban-column-container').css('padding', '10px');
+    // 這邊可以不用看，把預設Class改鰾文字才能真正居中，不知道為什麼
     $('.jqx-kanban-item-text').addClass('kanban-item-text')
     .removeClass('jqx-kanban-item-text');
     $('.jqx-kanban-item-color-status').remove();
+    // 調整大小
+    // $('#kanban').resizable({
+    //     alsoResize: '.jqx-kanban-column-container',
+    // });
 });
 
 function getRandom(min,max){
