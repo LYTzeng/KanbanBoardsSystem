f4d26e8e7174f04ac4d4eaae82ff83647e5b043e
Oscar Tzeng <oscar217b@gmail.com>
2018-12-18 22:55:36 +0800

    專案管理子系統PMS 完成並通過測試

---
+++ kanban/PMS/src/project.py
@@ -0,0 +1,115 @@
+"""專案管理子系統
+建立專案、查詢專案成員、權限、專案Metadata 等
+"""
+from typing import ( List, Dict )
+
+
+class Project():
+    def __init__(self, Firebase):
+        """初始化
+        :param Firebase: Firebase 的 API 認證 Class
+        """
+        self.firebase = Firebase
+        self.database = self.firebase.firestore()  # Firestore
+        self.project_db = self.database.collection('projects')
+
+        self.project_document = None
+        self.project_id = None
+        self.name = None
+        self.owner = None
+        self.members = None
+        self.columns = {'todo': list(), 'progress': list(), 'done': list()}
+        self.attr = ['todo', 'progress', 'done']
+
+    def create(self, request):
+        """建立全新專案"""
+        self.name = request.POST.get('name')
+        self.owner = request.POST.get('owner')
+        try:
+            self.members = request.POST.get('members')  # type: List[str]
+        finally:  # 一開始建立專案可以不加入其他人
+            pass
+
+        project_data = {
+            'name': self.name,  # type: str
+            'owner': self.owner,  # type: str
+            'members': self.members,  # type: List[str]
+            'columns': self.columns  # type: Dict[str: List[str]]
+        }
+        ref = self.project_db.document()
+        self.project_id = ref.id
+        ref.set(project_data)
+        self.get_board(project_id=ref.id)
+
+    def add_members(self, request):
+        """在舊專案新增成員"""
+        members_to_add = request.POST.get('members-to-add')  # type: List[str]
+        self.members.extend(members_to_add)
+        self.project_document.update(
+            {'members': self.members}
+        )
+
+    def delete_member(self, request):
+        """在舊專案移除成員"""
+        member_to_del = request.POST.get('member-to-delete')  # type: str
+        try:
+            self.members.remove(member_to_del)
+        except ValueError:
+            return "The member has been kicked out!"
+        self.project_document.update(
+            {'members': self.members}
+        )
+
+    def rename(self, request):
+        """重新命名專案"""
+        new_name = request.POST.get('new-name')
+        self.project_document.update(
+            {'name': new_name}
+        )
+
+    def get_board(self, request = None, project_id: str = str()):
+        """開啟舊專案"""
+        if request is not None:
+            self.project_id = request.POST.get('project-id')
+        elif project_id != str():
+            self.project_id = project_id
+        else:
+            raise ValueError
+        self.project_document = self.project_db.document(self.project_id)
+        project_data = self.project_document.get().to_dict()
+        self.name = project_data['name']
+        self.owner = project_data['owner']
+        self.members = project_data['members']
+        self.columns = project_data['columns']
+        return project_data  # type: dict
+
+    def move_task(self, task_id: str, src: str, dst: str):
+        """
+        在看板上移動任務
+        :param task_id:
+        :param src:
+        :param dst:
+        :return:
+        """
+        # TODO: 整合jqwidget API的動作
+        if src and dst not in self.attr:
+            raise ValueError
+        self.columns[src].remove(task_id)
+        self.columns[dst].append(task_id)
+        self.project_document.update({'columns': self.columns})
+
+    def add_task(self, task_id: str, column_to_add_to: str):
+        if column_to_add_to not in self.attr:
+            raise ValueError
+        self.columns[column_to_add_to].append(task_id)
+        self.project_document.update({'columns': self.columns})
+
+    def del_task(self, task_id: str, column_to_delete_from: str):
+        if column_to_delete_from not in self.attr:
+            raise ValueError
+        self.columns[column_to_delete_from].remove(task_id)
+        self.project_document.update({'columns': self.columns})
+
+    def delete_project(self):
+        self.project_document.delete()
+        self.__init__(self.firebase)
---
+++ kanban/PMS/test/pms_test.py
@@ -0,0 +1,115 @@
+import unittest
+from ..src.project import Project
+from kanban.testing.mock import Request
+from kanban.firebase.setup import Firebase
+
+
+class TestProject(unittest.TestCase):
+    firebase = Firebase()
+    # 建立一個假的Django request
+    request_generator = Request()
+
+    def setUp(self):
+        # 測試create()所需的User class
+        # 建立一個假的Firebase User
+        self.new_project = Project(self.firebase)
+        self.existed_project = Project(self.firebase)
+        self.request = self.request_generator.generate()
+        name = "existed_project"
+        owner = "success123"
+        members = ['member1', 'member2']
+        self.request.POST = {
+            'name': name,
+            'owner': owner,
+            'members': members
+        }
+        self.existed_project.create(self.request)
+        self.existed_project_id = self.existed_project.project_id
+
+    def tearDown(self):
+        if self.new_project.project_document is not None:
+            self.new_project.delete_project()
+        del self.new_project
+        self.existed_project.delete_project()
+        del self.existed_project
+        del self.existed_project_id
+        del self.request
+
+    def test_create(self):
+        name = "test_create_project"
+        owner = "success123"
+        self.request.POST = {
+            'name': name,
+            'owner': owner
+        }
+        self.new_project.create(self.request)
+        project_id = self.new_project.project_id
+        self.assertEqual(name, self.new_project.name)
+        self.assertEqual(owner, self.new_project.owner)
+        self.new_project.get_board(project_id=self.new_project.project_id)
+        self.assertEqual(name, self.new_project.name)
+        self.assertEqual(owner, self.new_project.owner)
+        self.assertEqual(project_id, self.new_project.project_id)
+
+    def test_add_members(self):
+        members_to_add = ['member3', 'member4']
+        self.request.POST = {
+            'members-to-add': members_to_add
+        }
+        self.existed_project.add_members(self.request)
+        expected = ['member1', 'member2', 'member3', 'member4']
+        self.existed_project.get_board(project_id=self.existed_project_id)
+        self.assertEqual(expected, self.existed_project.members)
+
+    def test_delete_member(self):
+        member_to_delete = 'member1'
+        self.request.POST = {
+            'member-to-delete': member_to_delete
+        }
+        self.existed_project.delete_member(self.request)
+        expected = ['member2']
+        self.existed_project.get_board(project_id=self.existed_project_id)
+        self.assertEqual(expected, self.existed_project.members)
+
+    def test_rename(self):
+        new_name = 'project one'
+        self.request.POST = {'new-name': new_name}
+        self.existed_project.rename(self.request)
+        self.existed_project.get_board(project_id=self.existed_project_id)
+        self.assertEqual(new_name, self.existed_project.name)
+
+    def test_get_board(self):
+        existed_project_id = self.existed_project.project_id
+        self.request.POST = {'project-id': existed_project_id}
+        self.new_project.get_board(self.request)
+        self.assertEqual(self.existed_project.name, self.new_project.name)
+        self.assertEqual(self.existed_project.owner, self.new_project.owner)
+        self.assertEqual(self.existed_project.columns, self.new_project.columns)
+        self.assertEqual(self.existed_project.members, self.new_project.members)
+        self.assertEqual(self.existed_project_id, self.new_project.project_id)
+
+    def test_move_task(self):
+        self.existed_project.add_task('A9zmCsIcad4AqWwNx3T6', 'todo')
+        self.existed_project.add_task('O3cbdiiF8J2jRdM11rAD', 'todo')
+        self.existed_project.get_board(project_id=self.existed_project_id)
+        self.existed_project.move_task('A9zmCsIcad4AqWwNx3T6', 'todo', 'progress')
+        self.existed_project.get_board(project_id=self.existed_project_id)
+        self.assertEqual('A9zmCsIcad4AqWwNx3T6', self.existed_project.columns['progress'][0])
+
+    def test_add_task(self):
+        self.existed_project.add_task('A9zmCsIcad4AqWwNx3T6', 'todo')
+        self.existed_project.get_board(project_id=self.existed_project_id)
+        self.assertEqual('A9zmCsIcad4AqWwNx3T6', self.existed_project.columns['todo'][0])
+
+    def test_del_task(self):
+        self.existed_project.add_task('A9zmCsIcad4AqWwNx3T6', 'todo')
+        self.existed_project.add_task('O3cbdiiF8J2jRdM11rAD', 'todo')
+        self.existed_project.get_board(project_id=self.existed_project_id)
+        self.assertEqual('O3cbdiiF8J2jRdM11rAD', self.existed_project.columns['todo'][1])
+        self.existed_project.del_task('O3cbdiiF8J2jRdM11rAD', 'todo')
+        self.existed_project.get_board(project_id=self.existed_project_id)
+        self.assertEqual('A9zmCsIcad4AqWwNx3T6', self.existed_project.columns['todo'][0])
+
+
+if __name__ == "__main__":
+    unittest.main()
